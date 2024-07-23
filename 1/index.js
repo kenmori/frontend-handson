8ae10c7ab83e087af667c90a562de2df4a045bb9
saco1208 <65397944+sunacodesu@users.noreply.github.com>
2024-07-23 22:40:14 +0900

    feat: add li to ul

--- 1/index.js
+++ 1/index.js
@@ -1,1 +1,10 @@
-console.log('1!');
+//get:ID='js-lists'
+const ul = document.getElementById('js-lists');
+
+//create <li></li>
+const li = document.createElement('li');
+
+//add:text -> これです
+li.textContent = 'これです';
+
+ul.appendChild(li);
