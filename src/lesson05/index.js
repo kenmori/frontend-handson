fe610ae1b2ce4fd4e137cedde9d6a875be69342a
saco1208 <65397944+sunacodesu@users.noreply.github.com>
2024-08-06 20:40:19 +0900

    add:lesson05

---
+++ src/lesson05/index.js
@@ -0,0 +1,30 @@
+const ul = document.getElementById("js-lists");
+const fragment = document.createDocumentFragment();
+
+const contents =   [
+    { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
+    { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" },
+];
+
+contents.forEach (content => {
+  const image = document.createElement('img');
+  const anchor = document.createElement('a');
+  const li = document.createElement('li');
+
+  image.src = content.img;
+  image.alt = content.alt;
+  anchor.href = `./${content.to}`;
+
+  fragment.appendChild(li)
+          .appendChild(anchor)
+          .appendChild(image)
+          .insertAdjacentText('afterend',content.text);
+
+  // Haru told me↓↓
+  // anchor.textContent = content.text
+  // fragment.appendChild(li)
+  //         .appendChild(anchor)
+  //         .insertAdjacentElement('afterbegin',img);
+})
+
+ul.appendChild(fragment);
